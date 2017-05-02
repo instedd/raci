7723e54357a8915136e3ae2e06bbd44dc5643c22
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2017-05-02 12:37:03 -0300

    Added default url options for devise mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "raci_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Devise mailer
+  config.action_mailer.default_url_options = { host: ENV["HOST"], port: ENV["PORT"] }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
