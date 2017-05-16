50c6976835dc3b76698fc8896473a05aeaef5c51
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2017-05-16 16:26:55 -0300

    Added translation for calendars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,16 @@ $(document).on('turbolinks:load', function() {
 
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+    monthsShort: [ 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic' ],
+    weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+    weekdaysShort: [ 'Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab' ],
+    today: 'Hoy',
+    clear: 'Limpiar',
+    close: 'Cerrar',
+    weekdaysLetter: [ 'D', 'L', 'M', 'X', 'J', 'V', 'S' ],
+    format: 'dd/mm/yyyy',
   });
   Materialize.updateTextFields();
 })
