e4dfc7c4be3fe5789722db7a74ec58d9e77194f4
pmallol <pmallol@manas.com.ar>
2017-05-22 18:18:31 -0300

    Fixed logo, font size and margins for ods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,20 +18,20 @@ module ApplicationHelper
   def ods(number, opts, &block)
     haml_tag :div, class: 'row' do
       haml_tag :div, class: "sdg-header ods#{number}" do
-        haml_tag :div, class: 'svg-container col m4 s12 hide-on-small-only' do
+        haml_tag :div, class: 'svg-container col m2 s12 hide-on-small-only' do
           haml_tag :svg, class: "logo ods#{number}" do
             haml_tag :use, "xlink:href" => "#ODS#{number}", class: "inverted"
           end
         end
-        haml_tag :div, class: 'sdg-content col m8 s11' do
+        haml_tag :div, class: 'sdg-content col m10 s12' do
           haml_tag :div, opts[:title], class: 'title'
           haml_tag :div, opts[:lead], class: 'lead'
         end
       end
     end
     haml_tag :div, class: 'targets row' do
-      haml_tag :div, class: 'col m4'
-      haml_tag :div, class: 'col m8 s12 content' do
+      haml_tag :div, class: 'col m2'
+      haml_tag :div, class: 'col m10 s12 content' do
         block.call
       end
     end
